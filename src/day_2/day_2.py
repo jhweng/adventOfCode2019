50119dbf6d460d0ddf9f0fb0e65efbbfbede6bb5
Juei Weng <juei.weng@inf.ufrgs.br>
2019-12-19 21:14:59 -0300

    Day 2: part 1 finished. Ok

--- src/day_2/day_2.py
+++ src/day_2/day_2.py
@@ -5,27 +5,33 @@ def main():
 
             size = 4
             for input_line in str_lst_input:
-                single_program_line = [int(elem.strip()) for elem in input_line.split(',')]
+                lst_program = [int(elem.strip()) for elem in input_line.split(',')]
 
-                # split_program_lines = [single_program_line[i:i+size] for i in range(0,len(single_program_line), size)]
-                print(single_program_line)
-                # print(split_program_lines)
-                for i in range(0, len(single_program_line), size):
-                    if single_program_line[i] == 1:  # add
-                        single_program_line[i+3] = single_program_line[i+1] + single_program_line[i+2]
-                        print('soma')
-                    elif single_program_line[i] == 2:  # multiply
-                        single_program_line[i + 3] = single_program_line[i + 1] * single_program_line[i + 2]
-                        print('multiplica')
-                    elif single_program_line[i] == 99:  # halt
-                        print('break')
+                # print(lst_program)
+                lst_program[1] = 12
+                lst_program[2] = 2
+
+                for i in range(0, len(lst_program), size):
+                    opcode = lst_program[i]
+
+                    if opcode == 99:
                         break
+
+                    operand_1_index = lst_program[i+1]
+                    operand_2_index = lst_program[i+2]
+                    dst_index = lst_program[i+3]
+                    
+                    if opcode == 1:  # add
+                        lst_program[dst_index] = lst_program[operand_1_index] + lst_program[operand_2_index]
+                        # print('soma')
+                    elif opcode == 2:  # multiply
+                        lst_program[dst_index] = lst_program[operand_1_index] * lst_program[operand_2_index]
+                        # print('multiplica')
                     else:
                         print('Error during decoding program!')
 
-                print(single_program_line)
-                print(single_program_line[0])
-
+                # print(lst_program)
+                print(lst_program[0])
 
     except IOError:
         print('Could not read input as \'day_2_input_txt\'')
@@ -33,3 +39,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
