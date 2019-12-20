47d1fa6dbb6303beee5569831d3c2848020dc43c
Juei Weng <juei.weng@inf.ufrgs.br>
2019-12-19 22:29:30 -0300

    Day 2 - part 2 completed!

--- src/day_2/day_2.py
+++ src/day_2/day_2.py
@@ -1,6 +1,6 @@
-def main():
+def int_program(noun, verb):
     try:
-        with open('day_2_input.txt') as f:
+        with open('day_2_input2.txt') as f:
             str_lst_input = f.readlines()
 
             size = 4
@@ -8,8 +8,8 @@ def main():
                 lst_program = [int(elem.strip()) for elem in input_line.split(',')]
 
                 # print(lst_program)
-                lst_program[1] = 12
-                lst_program[2] = 2
+                lst_program[1] = noun
+                lst_program[2] = verb
 
                 for i in range(0, len(lst_program), size):
                     opcode = lst_program[i]
@@ -17,10 +17,10 @@ def main():
                     if opcode == 99:
                         break
 
-                    operand_1_index = lst_program[i+1]
-                    operand_2_index = lst_program[i+2]
-                    dst_index = lst_program[i+3]
-                    
+                    operand_1_index = lst_program[i + 1]
+                    operand_2_index = lst_program[i + 2]
+                    dst_index = lst_program[i + 3]
+
                     if opcode == 1:  # add
                         lst_program[dst_index] = lst_program[operand_1_index] + lst_program[operand_2_index]
                         # print('soma')
@@ -30,11 +30,21 @@ def main():
                     else:
                         print('Error during decoding program!')
 
-                # print(lst_program)
-                print(lst_program[0])
+                return lst_program[0]
 
     except IOError:
         print('Could not read input as \'day_2_input_txt\'')
+
+
+def main():
+    for noun in range(100):
+        for verb in range(100):
+            output = int_program(noun, verb)
+            if output == 19690720:
+                print('noun= ', noun)
+                print('verb= ', verb)
+                print('100 * noun + verb = ', str(100 * noun + verb))
+                return
 
 
 if __name__ == '__main__':
